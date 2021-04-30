7eebec1d40732fbdb188f46d71f6babd5193e03e
ssanisidro <sergio.sanisidro.95@gmail.com>
2021-04-30 23:47:32 +0200

    PEC2

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,31 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
+/** importamos las dependencias de Bootstrap */
+
+import * as bootstrap from 'bootstrap';
+
++(function () {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+})();
+
+let menu_mobile = document.getElementById("header__hamburger-menu");
+
+menu_mobile.addEventListener('click', function () {
+  const header = document.getElementsByClassName('header')
+  header[0].classList.toggle("pushed");
+  const main_content = document.getElementsByClassName('main-content')
+  main_content[0].classList.toggle('main-pushed');
+  const bar = document.getElementsByClassName('bar');
+  bar[0].classList.toggle("animate");
+}, false);
